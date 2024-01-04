d2cc5f943925dd8f74903397489431a2da27cc37
root <root@v133-18-242-137.vir.kagoya.net>
2024-01-04 12:34:56 +0900

    これからIdeaNotesつくってくよ

---
+++ app/Http/Controllers/IdeaNotesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IdeaNotes;
+use Illuminate\Http\Request;
+
+class IdeaNotesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IdeaNotes $ideaNotes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(IdeaNotes $ideaNotes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, IdeaNotes $ideaNotes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IdeaNotes $ideaNotes)
+    {
+        //
+    }
+}
